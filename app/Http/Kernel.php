a128e87afee0b6d66e598bef9458279b9c03798e
andreramosneves <andreramosneves@yahoo.com.br>
2024-12-13 10:36:17 -0300

    Add Cors
    Adding Cors in application to run in Amazon

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends HttpKernel
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\EnsureJsonResponse::class, //JSON Only
         \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\Cors::class, //add Cors
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
